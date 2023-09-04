e7e7a2968e29a0ae7c0c03c9a7af81e4d94a3111
stranger-creator <raosuhas0@gmail.com>
2023-09-04 21:08:44 +0530

    done

--- frontend/src/Components/LoginButton.js
+++ frontend/src/Components/LoginButton.js
@@ -4,7 +4,23 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  const buttonStyle = {
+    position: "absolute",
+    top: "0",
+    right: "0",
+    margin: "10px", // Add margin for spacing if needed
+    padding: "5px 10px", // Add padding for button size
+    background: "blue", // Change the background color as desired
+    color: "white", // Change the text color as desired
+    border: "none",
+    cursor: "pointer",
+  };
+
+  return (
+    <button style={buttonStyle} onClick={() => loginWithRedirect()}>
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
--- frontend/src/Components/LogoutButton.js
+++ frontend/src/Components/LogoutButton.js
@@ -4,8 +4,20 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
+  const buttonStyle = {
+    position: "absolute",
+    top: "0",
+    right: "0",
+    margin: "10px", // Add margin for spacing if needed
+    padding: "5px 10px", // Add padding for button size
+    background: "red", // Change the background color as desired
+    color: "white", // Change the text color as desired
+    border: "none",
+    cursor: "pointer",
+  };
+
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button style={buttonStyle} onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
   );
