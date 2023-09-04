367069218f161085d9a3e31a59ace8795d1fa5c4
stranger-creator <raosuhas0@gmail.com>
2023-09-04 21:57:02 +0530

    done

--- frontend/src/Components/addpatient.js
+++ frontend/src/Components/addpatient.js
@@ -39,45 +39,45 @@ function AddPatient() {
   };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center', backgroundColor: '#f0f0f0', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
       <h2>Add Patient</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Name:</label>
+      <form onSubmit={handleSubmit} style={{ maxWidth: '400px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px', backgroundColor: '#fff' }}>
+        <div style={{ marginBottom: '16px' }}>
+          <label style={{ fontWeight: 'bold', marginBottom: '8px', color: '#333' }}>Name:</label>
           <input
             type="text"
             name="name"
             value={patient.name}
             onChange={handleInputChange}
-            className="form-control"
+            style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
             required
           />
         </div>
-        <div className="form-group">
-          <label>Disease:</label>
+        <div style={{ marginBottom: '16px' }}>
+          <label style={{ fontWeight: 'bold', marginBottom: '8px', color: '#333' }}>Disease:</label>
           <input
             type="text"
             name="disease"
             value={patient.disease}
             onChange={handleInputChange}
-            className="form-control"
+            style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
             required
           />
         </div>
-        <div className="form-group">
-          <label>Medication:</label>
+        <div style={{ marginBottom: '16px' }}>
+          <label style={{ fontWeight: 'bold', marginBottom: '8px', color: '#333' }}>Medication:</label>
           <input
             type="text"
             name="medication"
             value={patient.medication}
             onChange={handleInputChange}
-            className="form-control"
+            style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px' }}
             required
           />
         </div>
-        <div className="form-group">
-          <label>
-            Regular Checkup:
+        <div style={{ marginBottom: '16px' }}>
+          <label style={{ fontWeight: 'bold', marginBottom: '8px', color: '#333' }}>
+            Regular Checkup: &nbsp;&nbsp;
             <input
               type="checkbox"
               name="regularCheckup"
@@ -86,7 +86,7 @@ function AddPatient() {
             />
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" style={{ backgroundColor: '#007bff', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
           Add Patient
         </button>
       </form>
